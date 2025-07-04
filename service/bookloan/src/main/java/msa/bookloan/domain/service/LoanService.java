6120fdc2b606d2d9e977e15e61c7e37473e271d6
SunghoCha <tjdgh3725@gmail.com>
2025-07-04 21:46:25 +0900

    feat - loanService - loanBooks() 메서드 기초 구현 - 이벤트 발행, 레플리카 호출 관련 구현 예정

--- service/bookloan/src/main/java/msa/bookloan/domain/service/LoanService.java
+++ service/bookloan/src/main/java/msa/bookloan/domain/service/LoanService.java
@@ -2,28 +2,58 @@ package msa.bookloan.domain.service;
 
 import lombok.RequiredArgsConstructor;
 import msa.bookloan.adaptor.outbound.persistence.LoanRepository;
+import msa.bookloan.domain.model.BookLoan;
 import msa.bookloan.domain.model.LoanStatus;
+import msa.bookloan.domain.policy.LoanLimitPolicy;
+import msa.bookloan.domain.policy.LoanTermPolicy;
+import msa.bookloan.domain.policy.rule.LoanValidationRule;
 import msa.bookloan.dto.LoanCommand;
-import msa.bookloan.exception.LoanOverdueException;
+import msa.bookloan.dto.LoanContext;
+import msa.bookloan.exception.LoanLimitExceededException;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class LoanService {
 
     private final LoanRepository loanRepository;
+    private final List<LoanValidationRule> rules;
+    private final LoanLimitPolicy loanLimitPolicy;
+    private final LoanTermPolicy loanTermPolicy;
 
-    public void loanBook(LoanCommand loanCommand) {
-        /*
-            대출가능한지 체크
-            연체중인지/ 대출권수 초과인지
-            대출체크에필요한 조건은 동기로 호출
-       */
-        // 대출 조건 체크. 그런데 연체조건이라는기 인터페이스 기반 전략인데..
-        // 일단 간단한 구조부터 시도해보자. 처음부터 인터페이스 떠올리지말자
-        // 가장 기본적인건 일단 여러 전략을 생각하지말고 당장의 연체조건만 체크. 다른 인터페이스없이 레파지토리에서 조회해서 조건체크.
-        if (loanRepository.existsByMemberIdAndLoanStatus(loanCommand.memberId(), LoanStatus.OVERDUE)) {
-            throw new LoanOverdueException();
-        };
+    public void loanBooks(LoanCommand command) {
+        // 검증용 컨텍스트 객체
+        LoanContext context = LoanContext.builder()
+                .memberId(command.memberId())
+                .memberGrade(command.memberGrade())
+                .bookIds(command.bookIds())
+                .build();
+
+        rules.forEach(rule -> rule.validate(context));
+
+        List<BookLoan> bookLoans = command.bookIds().stream()
+                .map(id -> BookLoan.builder()
+                        .memberId(context.memberId())
+                        .bookId(id)
+                        .loanStatus(LoanStatus.LOANED)
+                        .bookCategory()
+                        .loanDate(LocalDate.now())
+                        .dueDate(LocalDate.now().plus(loanTermPolicy.loanPeriodFor()))
+                        .returnDate()
+                        .build())
+                .toList();
+
+        // 레파지토리 저장 수행...
+
+
+        List<BookLoan> results = loanRepository.saveAll(bookLoans);
+
+        return
+
+
     }
 }
