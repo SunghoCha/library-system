5a4a166c0816e608007fcc46df50223b32867070
SunghoCha <tjdgh3725@gmail.com>
2025-07-04 21:40:57 +0900

    feat - loanService - BookLoan 필드 추가 및 BookCategory 클래스 구현

--- service/bookloan/src/main/java/msa/bookloan/domain/model/BookLoan.java
+++ service/bookloan/src/main/java/msa/bookloan/domain/model/BookLoan.java
@@ -5,23 +5,45 @@ import jakarta.persistence.Id;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import msa.common.domain.BaseTimeEntity;
+import msa.common.domain.BookCategory;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @RequiredArgsConstructor
-public class BookLoan {
+public class BookLoan extends BaseTimeEntity {
 
     @Id
     private Long id;
 
     private Long memberId;
 
+    private Long bookId;
+
     private LoanStatus loanStatus;
 
+    private BookCategory bookCategory;
+
+    private LocalDateTime loanDate;
+
+    private LocalDateTime dueDate;
+
+    private LocalDateTime returnDate;
+
+
     @Builder
-    public BookLoan(Long id, Long memberId, LoanStatus loanStatus) {
+    public BookLoan(Long id, Long memberId, Long bookId, LoanStatus loanStatus, LocalDateTime loanDate,
+                    LocalDateTime dueDate, LocalDateTime returnDate, BookCategory bookCategory) {
         this.id = id;
         this.memberId = memberId;
+        this.bookId = bookId;
         this.loanStatus = loanStatus;
+        this.loanDate = loanDate;
+        this.dueDate = dueDate;
+        this.returnDate = returnDate;
+        this.bookCategory = bookCategory;
     }
 }
