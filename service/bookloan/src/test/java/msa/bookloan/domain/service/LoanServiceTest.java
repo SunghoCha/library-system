aad3a9f602fe8907a40aef3801122d58a30a2916
SunghoCha <tjdgh3725@gmail.com>
2025-07-04 21:45:22 +0900

    refactor - loanService - 대출 테스트 로직 리팩토링 - 책임이 분리되면서 기존의 검증 로직이 Rule클래스로 이동됨에 따른 리팩토링

--- service/bookloan/src/test/java/msa/bookloan/domain/service/LoanServiceTest.java
+++ service/bookloan/src/test/java/msa/bookloan/domain/service/LoanServiceTest.java
@@ -2,9 +2,13 @@ package msa.bookloan.domain.service;
 
 import msa.bookloan.adaptor.outbound.persistence.LoanRepository;
 import msa.bookloan.domain.model.LoanStatus;
+import msa.bookloan.domain.policy.LoanLimitPolicy;
+import msa.bookloan.domain.policy.rule.LoanValidationRule;
 import msa.bookloan.dto.LoanCommand;
+import msa.bookloan.exception.LoanLimitExceededException;
 import msa.bookloan.exception.LoanOverdueException;
 import msa.common.domain.MemberGrade;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,10 +16,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 
 @ExtendWith(MockitoExtension.class)
@@ -24,25 +31,72 @@ class LoanServiceTest {
     @Mock
     private LoanRepository loanRepository;
 
+    @Mock
+    private LoanValidationRule overdueRule;
+
+    @Mock
+    private LoanValidationRule loanLimitRule;
+
+    @Mock
+    private LoanLimitPolicy loanLimitPolicy;
+
     @InjectMocks
     private LoanService loanService;
 
     private final Long MEMBER_ID = 1L;
     private final Long BOOK_ID = 42L;
 
+    @BeforeEach
+    void setUp() {
+        loanService = new LoanService(
+                loanRepository,
+                List.of(overdueRule, loanLimitRule),
+                loanLimitPolicy
+        );
+    }
+
     @Test
-    @DisplayName("만약 회원이 연체 중인 도서가 하나라도 있으면 → 대출 거부")
-    void test() {
+    @DisplayName("조건을 만족하면 대출 승인")
+    void loanBook_with_correct_input() {
         // given
         given(loanRepository.existsByMemberIdAndLoanStatus(MEMBER_ID, LoanStatus.OVERDUE))
-                .willReturn(true);
+                .willReturn(false);
+        // when
+
+        // then
+    }
+
+    @Test
+    @DisplayName("회원이 연체 중인 도서가 하나라도 있으면 대출 거부")
+    void loanBook_with_wrong_input_about_overdue() {
+        // given
+        doThrow(new LoanOverdueException())
+                .when(overdueRule)
+                .validate(any());
         // when & then
         LoanCommand command = new LoanCommand(MEMBER_ID, MemberGrade.GOLD, BOOK_ID);
-        assertThatThrownBy(() -> loanService.loanBook(command))
+        assertThatThrownBy(() -> loanService.loanBooks(command))
                 .isInstanceOf(LoanOverdueException.class)
                 .hasMessage("연체 중인 도서가 있습니다.");
-        // 실제로 레파지토리가 조회가 한 번 호출됐는지 검증
-        then(loanRepository).should(times(1))
-                .existsByMemberIdAndLoanStatus(MEMBER_ID, LoanStatus.OVERDUE);
+
+        then(overdueRule).should(times(1)).validate(any());
     }
+
+    @Test
+    @DisplayName("회원이 대출한도를 초과했으면 대출 거부")
+    void loanBook_with_wrong_input_about_loan_limit() {
+        // given
+        doThrow(new LoanLimitExceededException())
+                .when(loanLimitRule)
+                .validate(any());
+        // when & then
+        LoanCommand command = new LoanCommand(MEMBER_ID, MemberGrade.GOLD, BOOK_ID);
+        assertThatThrownBy(() -> loanService.loanBooks(command))
+                .isInstanceOf(LoanLimitExceededException.class)
+                .hasMessage("대출 한도를 초과했습니다.");
+
+        then(loanLimitRule).should(times(1)).validate(any());
+    }
+
+
 }
